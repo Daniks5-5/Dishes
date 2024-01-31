cf430ce3e830b3e6b412df887574795339c1d756
123 <b22@mail.ru>
2024-01-31 13:56:43 +0300

    first commit

---
+++ Dishes/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dishes
+{
+    public class Helper
+    {
+        public static TableWareEntities context = new TableWareEntities();
+    }
+}
+class Test
+{
+
+}
